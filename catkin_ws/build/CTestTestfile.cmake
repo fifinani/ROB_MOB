86ae0bcac98c13e1b0ae3f6e464cb676ec3a24cb
firas <firasbengaied@hotmail.fr>
2019-11-15 15:03:53 +0100

    Commit Initial

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,13 @@
+# CMake generated Testfile for 
+# Source directory: /home/firas/ROB_MOB/catkin_ws/src
+# Build directory: /home/firas/ROB_MOB/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(joystick_drivers/joystick_drivers)
+subdirs(my_teleop)
+subdirs(joystick_drivers/joy)
+subdirs(joystick_drivers/ps3joy)
+subdirs(joystick_drivers/spacenav_node)
+subdirs(joystick_drivers/wiimote)
---
+++ catkin_ws/build/my_teleop/CMakeFiles/my_teleop.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_teleop.dir/src/my_teleop.cpp.o"
+  "/home/firas/ROB_MOB/catkin_ws/devel/lib/my_teleop/my_teleop.pdb"
+  "/home/firas/ROB_MOB/catkin_ws/devel/lib/my_teleop/my_teleop"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_teleop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
